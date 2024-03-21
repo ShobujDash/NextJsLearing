b3dae6f3765f8a1a8ba0cdf337dabbaf6c242ac2
ShobujDas <shobujd6.gmail.com>
2024-03-22 01:52:33 +0600

    added loading

---
+++ app/heavy/page.jsx
@@ -0,0 +1,63 @@
+export default function Heavy() {
+  return (
+    <div>
+      <h1>Heavy Page</h1>
+      
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
+        corporis ab adipisci, harum consequuntur ut numquam sequi excepturi
+        laudantium hic aspernatur! Esse distinctio fugiat unde animi dicta, non
+        exercitationem laborum.
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
+        corporis ab adipisci, harum consequuntur ut numquam sequi excepturi
+        laudantium hic aspernatur! Esse distinctio fugiat unde animi dicta, non
+        exercitationem laborum.
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
+        corporis ab adipisci, harum consequuntur ut numquam sequi excepturi
+        laudantium hic aspernatur! Esse distinctio fugiat unde animi dicta, non
+        exercitationem laborum.
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
+        corporis ab adipisci, harum consequuntur ut numquam sequi excepturi
+        laudantium hic aspernatur! Esse distinctio fugiat unde animi dicta, non
+        exercitationem laborum.
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
+        corporis ab adipisci, harum consequuntur ut numquam sequi excepturi
+        laudantium hic aspernatur! Esse distinctio fugiat unde animi dicta, non
+        exercitationem laborum.
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
+        corporis ab adipisci, harum consequuntur ut numquam sequi excepturi
+        laudantium hic aspernatur! Esse distinctio fugiat unde animi dicta, non
+        exercitationem laborum.
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
+        corporis ab adipisci, harum consequuntur ut numquam sequi excepturi
+        laudantium hic aspernatur! Esse distinctio fugiat unde animi dicta, non
+        exercitationem laborum.
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
+        corporis ab adipisci, harum consequuntur ut numquam sequi excepturi
+        laudantium hic aspernatur! Esse distinctio fugiat unde animi dicta, non
+        exercitationem laborum.
+      </div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
+        corporis ab adipisci, harum consequuntur ut numquam sequi excepturi
+        laudantium hic aspernatur! Esse distinctio fugiat unde animi dicta, non
+        exercitationem laborum.
+      </div>
+      
+    </div>
+  );
+}
